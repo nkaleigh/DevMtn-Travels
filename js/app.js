856f3bd2d06833e7eafa79dc4d1f3c5b25a57a57
Kaleigh Niemela <nkaleigh@users.noreply.github.com>
2017-04-03 14:32:10 -0600

    first

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 angular.module('devmtnTravel', ['ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('home',{
-                url:'/',
+            .state('home', {
+                url: '/',
                 templateUrl: "../views/about.html"
-            });
-
+            })
+            .state('aboutAdventures', {
+                url: '/aboutAdventures',
+                templateUrl: '../views/about-adventurers.html'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: '../views/about.html'
+            })
+            .state('booked', {
+                url: '/booked',
+                templateUrl: '../views/booked.html'
+            })
+            .state('contact', {
+                url: '/contact',
+                templateUrl: '../views/contact.html'
+            })
+            .state('locations', {
+                url: '/locations',
+                templateUrl: '../views/locations.html'
+            })
+            .state('packages', {
+                url: '/packages',
+                templateUrl: '../views/packages.html'
+            })
         $urlRouterProvider
             .otherwise('/');
     });
